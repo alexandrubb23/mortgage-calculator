918894817a886bc3ec326399ab9decc5c9786ff2
Alexandru Barbulescu <alexandru.barbulescu@vodafone.com>
2021-05-20 16:47:38 +0300

    Move all classes under a folder.

--- main.php
+++ main.php
@@ -1,8 +1,8 @@
 <?php
 
-require 'MortgageCalculator.php';
-require 'MortgageReport.php';
-require 'Console.php';
+require 'clasees/Console.php';
+require 'clasees/MortgageCalculator.php';
+require 'clasees/MortgageReport.php';
 
 $principal = Console::readNumber("Principal:", 1000, 1000_000);
 $annualInterest = Console::readNumber("Annual Interest Rate:", 1, 30);
